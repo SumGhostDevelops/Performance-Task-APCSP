29c1397e908a76b56df558bbd71f26ad93b76097
Vivi C <amanchowdhury08@gmail.com>
2025-04-03 09:47:30 -0500

    STOP CRASHING

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame f = new JFrame();
+        JFrame window = new JFrame();
+        JButton b1 = new JButton("Hello, World!");
+        JTextArea bal = new JTextArea("Hello");
 
-        f.setSize(400, 400);
-        f.setVisible(true);
+        window.setSize(400, 400);
+        window.setVisible(true);
+        window.setTitle("Sigma Game Hatsune Miku");
+        window.setResizable(false);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        b1.setSize(200,200);
+        b1.setVisible(true);
 
     }
 }
