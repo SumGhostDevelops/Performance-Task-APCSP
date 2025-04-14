6d61f60f1cc438d43d3e6abe7af2f8afbb4c16cf
Vivi C <69370002+SumGhostDevelops@users.noreply.github.com>
2025-04-14 02:35:24 -0500

    ITS MOVING ITS MOVING FINALLY AKSHFLKASDJLFASDLKGFLSJDGHSJLD

--- src/Panel.java
+++ src/Panel.java
@@ -19,7 +19,7 @@ public class Panel extends JPanel implements ActionListener {
     final int snakeX[] = new int[total_units]; //X positions of all snake bodyparts
     final int snakeY[] = new int[total_units]; //Y positions of all snake body parts
     int snake_body = 5; // starting snake body
-    char direction; // which direction the snake is facing
+    char direction = 'D'; // which direction the snake is facing
 
     //Apple variables
     int Score;
@@ -29,19 +29,71 @@ public class Panel extends JPanel implements ActionListener {
 
 
     Panel(){
-
+    r = new Random();
+    this.setPreferredSize(new Dimension(ScreenDim,ScreenDim));
+    this.setBackground(Color.black);
+    this.setFocusable(true);
+    this.addKeyListener(new KeyDetect());
+    Start();
     }
     public void Start(){
-
+        CreatePiece();
+        running = true;
+        t = new Timer(GLOBAL_OFFSET, this);
+        t.start();
     }
     public void move(){
+        for( int i = snake_body; i > 0; i--){
+            snakeX[i] = snakeX[i-1];
+            snakeY[i] = snakeY[i-1];
+        }
+        switch(direction){
+            case 'W':
+                snakeY[0] = snakeY[0] - unit;
+                break;
+            case 'S':
+                snakeY[0] = snakeY[0] + unit;
+                break;
+            case 'A':
+                snakeX[0] = snakeX[0] - unit;
+                break;
+            case 'D':
+                snakeX[0] = snakeX[0] + unit;
+                break;
 
+        }
     }
-    public void Paint(Graphics paint){
-
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
     }
-    public void Draw(Graphics draw){
+    public void draw(Graphics g) {
 
+        if (running) {
+
+            for (int i = 0; i <( ScreenDim / unit); i++) {
+
+                g.drawLine(i * unit, 0, i * unit, ScreenDim);
+                g.drawLine(0, i * unit, ScreenDim, i * unit);
+                g.setColor(new Color(105, 105, 105));
+            }
+            g.setColor(Color.yellow);
+            g.fillRect(appleX,appleY, unit,unit);
+
+            for(int i = 0 ; i < snake_body ; i++){
+                if (i != 0){
+                   g.setColor(new Color(135, 87, 186));
+                   g.fillRect(snakeX[i],snakeY[i], unit, unit);
+                } else{
+                    g.setColor(new Color(80, 47, 115));
+                    g.fillRect(snakeX[i],snakeY[i], unit, unit);
+                }
+            }
+        }
+    }
+    public void CreatePiece(){
+        appleX = r.nextInt((int)(ScreenDim/unit))*unit;
+        appleY = r.nextInt((int)(ScreenDim/unit))*unit;
     }
     public void CollisionChecker(){
 
@@ -54,7 +106,13 @@ public class Panel extends JPanel implements ActionListener {
     }
     @Override
     public void actionPerformed(ActionEvent e) {
+        if (running){
+            move();
+            AppleChecker();
+            CollisionChecker();
 
+        }
+        repaint();
     }
 
     public class KeyDetect extends KeyAdapter{
